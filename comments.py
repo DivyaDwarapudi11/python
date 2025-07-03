e082d00567cdc7144153524d4f7e0cef27d26743
DivyaDwarapudi11 <divyadwarapudi@gmail.com>
2025-07-03 09:50:24 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+which provide additional information about the coding
+
+they are two types
+
+1.single line (in line comments):
+    used to represent the programms information in a line represented by "#"
+
+2. multi line (block level comments)
+    used to represent the program information  of paragraph wise represented """"""
